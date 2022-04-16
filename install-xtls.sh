def735b781a28bdeda397ff98361cde2552afd74
huahsui <103646023+huahsui@users.noreply.github.com>
2022-04-16 15:43:49 +0800

    Update install-xtls.sh

--- install-xtls.sh
+++ install-xtls.sh
@@ -105,7 +105,7 @@ echo
 echo "   恭喜，你的tcp+xtls已配置成功，以下为你的clash配置"
 echo
 echo "----------------------------------------------------------------------------------------------------------------------------------------------"
-echo "- {name: tcp+xtls, server: $DOMIN, port: 443, type: vless, uuid: $UUID, flow: xtls-rprx-direct, skip-cert-verify: false,servername: $DOMIN}"
+echo "- {name: tcp+xtls, server: $DOMIN, port: 443, type: vless, uuid: $UUID, flow: xtls-rprx-direct, skip-cert-verify: false, servername: $DOMIN}"
 echo
 echo "   clash配置文件在 https://$DOMIN/$UUID.yaml ,请直接在clash客户端中输入该网址食用！！！"
 echo
