4dda601bfd06773ea69b9fc755bd91f8bac00302
划划水 <103646023+huahsui@users.noreply.github.com>
2022-05-09 20:14:17 +0800

    Update ws-tls.sh

--- ws-tls.sh
+++ ws-tls.sh
@@ -57,9 +57,9 @@ server {
                     proxy_pass                  http://127.0.0.1:24600; 
                     proxy_redirect              off;
                     proxy_http_version          1.1;
-                    proxy_set_header Upgrade    $http_upgrade;
+                    proxy_set_header Upgrade    /$http_upgrade;
                     proxy_set_header Connection "upgrade";
-                    proxy_set_header Host       $http_host;
+                    proxy_set_header Host       /$http_host;
            }
 } 
 server { 
